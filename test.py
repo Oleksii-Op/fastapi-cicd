5c9f9b58cf50e06370beabd3a8e975f3936b56b7
main <secret@gmail.com>
2024-10-27 13:57:19 +0200

    Mics

--- test.py
+++ test.py
@@ -8,6 +8,10 @@ def test_foo():
     res = 1
     assert res == 1
 
+def test_foo2():
+    res = {"message": "Hello World"}
+    assert res == {"message": "Hello World"}
+
 
 def test_root_page():
     response = client.get("/")
