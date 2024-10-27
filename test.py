cc5ee654e5729944d54066c6146f9ba42d198e0c
main <secret@gmail.com>
2024-10-27 12:59:10 +0200

    Simple tests

---
+++ test.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_foo():
+    res = 1
+    assert res == 1
+
+
+def test_root_page():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
