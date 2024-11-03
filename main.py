4da1644bf1fda8a3ff6ea5cb4c31f60731ea00b5
Oleksii-Op <oleksii.oprish@gmail.com>
2024-11-03 21:19:15 +0200

    Added /user endpoint

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ async def root_page():
     return {"message": "Hello World"}
 
 
+@app.get("/user")
+async def get_user():
+    return {
+        "id": "16fc42b4ac60aa4d31ef7a8bdee7903ac",
+        "username": "Random",
+        "email": "random@example.com",
+    }
+
+
 if __name__ == "__main__":
     configure_logger(level=logging.INFO)
     logger.info("Starting FastAPI app")
