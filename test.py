9e05a743af70b1ad7bfbaeae7aa47ae1cfae2668
Oleksii-Op <oleksii.oprish@gmail.com>
2024-11-03 21:19:26 +0200

    Added /user endpoint test

--- test.py
+++ test.py
@@ -18,3 +18,15 @@ def test_root_page():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_get_user():
+    correct_response = {
+        "id": "16fc42b4ac60aa4d31ef7a8bdee7903ac",
+        "username": "Random",
+        "email": "random@example.com",
+    }
+    response = client.get("/user")
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.json() == correct_response
